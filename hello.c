fd668b8485677fc00b5794c57e09b7ea1512a702
Uugan1904 <uuganaa1904@gmail.com>
2026-09-20 23:10:44 +0800

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int age = 19;
+    float Height = 5.5;
+    char Initial = 'U';
+    float weigth = 62.5;
+    float IELTS = 7.0;
+    printf(" %c is %d years old, ")
+}
